c820afdcc0293f063822e59af7eb435b92e17fa3
Yuto Tanaka <karaage_nennen@icloud.com>
2021-11-20 10:41:52 +0900

    Support v2 compose update

--- functions/update_docker_compose.fish
+++ functions/update_docker_compose.fish
@@ -3,11 +3,13 @@ function update_docker_compose --description 'Update docker-compose'
     if read_confirm
         set OLD_VERSION (docker-compose version --short)
         set VERSION (curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
-        set DESTINATION /usr/local/bin/docker-compose
+        set DESTINATIONS "/usr/local/bin/docker-compose" "$HOME/.docker/cli-plugins/docker-compose"
         if test $OLD_VERSION != $VERSION
             echo "Update found!"
-            sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-(uname -s)-(uname -m) -o $DESTINATION
-            sudo chmod 755 $DESTINATION
+            sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-(uname -s)-(uname -m) -o $DESTINATIONS[1]
+            sudo cp $DESTINATIONS[1] $DESTINATIONS[2]
+            sudo chmod 755 $DESTINATIONS[1]
+            sudo chmod 755 $DESTINATIONS[2]
         else
             echo "No update required."
         end
